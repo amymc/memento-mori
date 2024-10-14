5e922e9f8c763a1dca06c0a42d2cee2e3820b93c
amymc <mccarthy.amy.n@gmail.com>
2024-10-14 23:37:34 +0100

    use FIFO queue

--- server/server.ino
+++ server/server.ino
@@ -1,10 +1,7 @@
 #include <string>
 #include <ArduinoJson.h>
-// #include <ESP8266WiFi.h>
-// #include <WiFiClient.h>
+#include <queue>
 #include <ESP8266WebServer.h>
-// #include <ESP8266mDNS.h>
-
 #include "secrets.h"  // add WLAN Credentials in here.
 
 ESP8266WebServer server(80);
@@ -25,9 +22,16 @@ String grimReaper = "             ___\n"
                     "           |       \\\\\n"
                     "           |________|\\\n";
 
-int handleForm() {
-  server.send(200, "text/plain", "success message");
- 
+class Participant {
+  public:
+    String name; 
+    String expectancy;
+};
+
+Participant participant;
+std::queue<Participant> participantQueue;
+
+void handleForm() {
   DynamicJsonDocument doc(1024);
 
   String input = server.arg("plain");
@@ -35,10 +39,13 @@ int handleForm() {
 
   const String name = doc["name"];
   const String expectancy = doc["expectancy"];
-  return printMemento(name, expectancy);
+
+  participantQueue.push({ name: name, expectancy: expectancy });
+
+  server.send(200, "text/plain", "success");
 }
 
-int printMemento(String name, String expectancy) {
+void printMemento(String name, String expectancy) {
   String years = expectancy + " years";
   if (expectancy == "1"){
     years = expectancy + " year";
@@ -53,19 +60,10 @@ int printMemento(String name, String expectancy) {
   Serial.println("I'll be waiting for you.");
   Serial.println("********************************");
   Serial.println("********************************");
-
-  return 0;
 }
-
-// void handleListFiles() {
-//   Serial.println("list files");
-//   server.sendHeader("Cache-Control", "no-cache");
-//   server.send(200, "text/javascript; charset=utf-8", "result");
-// }  // handleListFiles()
 
 void setup()
 {
-
   // Serial.begin(19200);
   Serial.begin(9600);
 
@@ -83,50 +81,18 @@ void setup()
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // server.on("/$list", HTTP_GET, handleListFiles);
   server.on("/participants", HTTP_POST, handleForm);
-
   server.enableCORS(true);
-
-  // server.on("/$participants", HTTP_POST, handleForm, handleForm);
-  // server.on("/participants", []() {
-  //   server.send(200, "text/plain", "success result");
-  // });
-
   server.begin();
 }
 
 void loop()
 {
   server.handleClient();
-  //   while (!Serial) {
-  //   // wait for serial port to connect. Needed for native USB
-  // }
-  // if (Serial.available() > 0)
-  // {
 
-  //   String info = Serial.readString();
-  //   String strs[2];
-  //   int StringCount = 0;
-
-  //   int index = info.indexOf(' ');
-  //   strs[0] = info.substring(0, index);
-  //   strs[1] = info.substring(index + 1);
-
-  //   String years = strs[1] + " years";
-  //   if (strs[1] == "1")
-  //   {
-  //     years = strs[1] + " year";
-  //   }
-
-  //   String expiration = String("    You are due to expire in \n          " + years + ".");
-
-  //   Serial.println("\n********************************");
-  //   Serial.println(expiration);
-  //   Serial.print(grimReaper);
-  //   Serial.println("Enjoy it " + strs[0] + ".");
-  //   Serial.println("I'll be waiting for you.");
-  //   Serial.println("********************************");
-  //   Serial.println("********************************");
-  //}
+  while(!participantQueue.empty()) {
+    participant = participantQueue.front();
+    printMemento(participant.name, participant.expectancy);
+    participantQueue.pop();
+  }
 }
